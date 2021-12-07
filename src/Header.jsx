43a9b7ab7676a25fb52a3d8e4aecf5b8a9d03da2
Alisher778 <alisher.musurmonov89@gmail.com>
2021-12-07 11:04:32 +0500

    add: set up initial filesa and folders

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HeaderComponents() {
+  const demo = 8880;
+  return (
+    <header>
+      <h2>This is the header {demo}</h2>
+    </header>
+  )
+};
+
+export default HeaderComponents;
